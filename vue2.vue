3cfe394dcac44b8058bdeda62cd798dd5f85f19e
moli001 <131584612+moli001@users.noreply.github.com>
2023-06-19 19:01:55 -0300

    Update vue2.vue

--- vue2.vue
+++ vue2.vue
@@ -1,3 +1,32 @@
 <div id="app">
   <p>{{ message }}</p>
 </div>
+
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+ 
+for letter in 'Python':     # 第一个实例
+   if letter == 'h':
+      continue
+   print '当前字母 :', letter
+ 
+var = 10                    # 第二个实例
+while var > 0:              
+   var = var -1
+   if var == 5:
+      continue
+   print '当前变量值 :', var
+print "Good bye!"
+
+
+
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+n = 0
+while n < 10:
+    n = n + 1
+    if n % 2 == 0:      # 如果n是偶数，执行continue语句
+        continue        # continue语句会直接继续下一轮循环，后续的print()语句不会执行
+    print(n)
